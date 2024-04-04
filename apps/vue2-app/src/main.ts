36473af7b16cbdc395ea57b02d8dfa2ddf01ee24
ice breaker <1324318532@qq.com>
2024-04-05 00:43:10 +0800

    chore(deps): link pkg

--- apps/vue2-app/src/main.ts
+++ apps/vue2-app/src/main.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import './style.css'
+import vStickyPlugin from 'vue-sticky-next'
 import App from './App.vue'
+Vue.use(vStickyPlugin)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
